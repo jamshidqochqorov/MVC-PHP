cdd6cf5aa01e76780b7e2f06a2f96cfe605123f7
jamshid <jamshidqochqorov84@gmail.com>
2023-04-04 21:40:30 +0500

    added renderView

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ .'/../vendor/autoload.php';
 /**
  * User: Jamshid
  * Date:04.04.2023
@@ -7,13 +8,9 @@
 use app\core\Application;
 
 
-require_once __DIR__ .'/../vendor/autoload.php';
-$app = new Application();
 
-$app->router->get('/',function (){
-    return "Hello word";
-});
-$app->router->get('/contact',function (){
-    return "Contact";
-});
+$app = new Application(dirname(__DIR__));
+
+$app->router->get('/','home');
+$app->router->get('/contact','contact');
 $app->run();
